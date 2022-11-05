2ac4490ce58d773eda9c01a1ecf8eddcd81da634
guoanhu <8naouguh@gmail.com>
2022-11-05 11:06:37 +0800

    setup antd typescript environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from "react";
+import { Button, Card, Col, Row } from "antd";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => (
+
+
+  <div className="App">
+    <Row>
+      <Col span={12}>
+      <Card
+  
+      title={'策略'}
+      
+    >
+      {/* <div >hello</div> */}
+    </Card>
+      </Col>
+   
+   <Col span={12}>
+   
+   <Card
+    
+      title={'特征'}
+      
+    >
+      <div >hello</div>
+    </Card>
+
+   </Col>
+
+    </Row>
+  </div>
+
+);
 
 export default App;
